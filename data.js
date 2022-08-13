8a89653ea057a63c16bee44a5b9248ef79a203c6
Dev Mehta <55938019+Dev-Mehta@users.noreply.github.com>
2022-08-13 17:43:26 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+	{
+		name: 'Mera Yaar',
+		path: 'assets/music/mera_yaar.mp3',
+		artist: 'Shankar-Ehsaan-Loy, Javed Bashir',
+		cover: 'https://i.scdn.co/image/ab67616d0000b273fd2df008046f04c32d9c0c2e'
+	},
+	{
+		name: 'Kahin To',
+		path: 'assets/music/kahin_to.mp3',
+		artist: 'Rashid Ali, Vasundhara Das',
+		cover: 'https://i.scdn.co/image/ab67616d00001e02abf91851997179d195afab5f'
+	},
+	{
+		name: 'Tu Jaane Na',
+		path: 'assets/music/tu_jaane_na.mp3',
+		artist: 'Atif Aslam, Pritam',
+		cover: 'https://i.scdn.co/image/ab67616d00001e02f77a84e6e8fe67deecc242dd'
+	},
+	{
+		name: 'Aye Khuda',
+		path: 'assets/music/aye_khuda.mp3',
+		artist: 'Salim Merchant',
+		cover: 'https://i.scdn.co/image/ab67616d0000b27305ab1baaa49d516762821a20'
+	},
+	{
+		name: 'Bulleya',
+		path: 'assets/music/bulleya.mp3',
+		artist: 'Vishal-Shekhar, Papon, Irshad Kamil',
+		cover: 'https://i.scdn.co/image/ab67616d0000b273b2abfd2439b6c23c3664df87'
+	},
+	{
+		name: 'Aaoge Jab Tum',
+		path: 'assets/music/naina_tere_kajrare.mp3',
+		artist: 'Rashid Khan',
+		cover: 'https://i.scdn.co/image/ab67616d0000b27352fe6875028c892308ffc2f7'
+	},
+	{
+		name: 'Tajdar-e-Haram',
+		path: 'assets/music/tajdar_e_haram.mp3',
+		artist: 'Atif Aslam',
+		cover: 'https://i.scdn.co/image/ab67616d0000b273fb515071c18fd76ca59a5a05'
+	},
+	{
+		name: 'Tose Naina',
+		path: 'assets/music/tose_naina.mp3',
+		artist: 'Arijit Singh',
+		cover: 'https://i.scdn.co/image/ab67616d0000b27376d18b5a17bd977a9a87551b'
+	}
+]
